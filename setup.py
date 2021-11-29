7340ce120fe09a86a6d54cff5006ce9f76e61e89
shovelingpig <flux1126@gmail.com>
2021-11-29 08:18:26 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='decalib',
+   version='1.0',
+   description='A useful module',
+   author='',
+   author_email='',
+   packages=['decalib'],  #same as name
+)

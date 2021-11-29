0878b8ffc8d2305a42c474f705a4942ba97a0d0a
Minchan Kim <55829146+shovelingpig@users.noreply.github.com>
2021-11-29 17:56:31 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='decalib',
@@ -6,5 +6,6 @@ setup(
    description='A useful module',
    author='',
    author_email='',
-   packages=['decalib'],  #same as name
+   packages=find_packages(),
+   url="https://github.com/dhkwak/simple_scheduler"
 )
